900c0f81e034db71694b38eae4943d30c38f3ad2
Daniel Carneiro Miranda <daniel.miranda@caixa.gov.br>
2022-02-12 02:38:59 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\routes;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
+use League\CommonMark\Environment\Environment;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return [
+        App::environment(),
+        config('app.timezone')
+    ];
+});
